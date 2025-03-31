962ff2f034009048d99d04611706bdf0dcab7d8c
Dan131195 <daniele.franceschini1395@gmail.com>
2025-03-31 13:03:17 +0200

    cliente

---
+++ BuildWeek_Api/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuildWeek_Api.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public string CodiceFiscale { get; set; } 
+        public string? Nome { get; set; }
+        public string? Cognome { get; set; }
+
+        public ICollection<Vendita> Vendite { get; set; } 
+    }
+}
